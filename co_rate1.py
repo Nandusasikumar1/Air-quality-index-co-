1194311b1c3e04887c8502fa3a936266cf4fd73a
Nandu  s <74356710+Nandusasikumar1@users.noreply.github.com>
2021-04-05 10:16:20 +0530

    Update co_rate1.py

--- co_rate1.py
+++ co_rate1.py
@@ -9,25 +9,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 import numpy as np
-import time
 class air:
     def airquality(self):
         url='https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key=579b464db66ec23bdd0000013f43dd62e27448bd6759892c8c5d4bfa&format=csv&offset=0&limit=2000'
         response=requests.get(url,params=None)
-        file=open('co_india.csv','w')
-        file.write(response.text)
-        file.close()
+        k=[i.split(',') for i in  response.text.split('\n')]
+        k.remove(k[0])
+        df=pd.DataFrame(k)
+        df1=df[[2,3,7,10]]
+        df1.rename(columns=dict(zip(list(df1.columns),['state','city','pollutant','pollutant_avg'])),inplace=True)
+        return df1
+
     def auto_co_visualizer(self):
-        self.airquality()
-        file=pd.read_csv('co_India.csv')
-        file.drop('id',axis=1,inplace=True)
-        f1=file[file['pollutant_id']=='CO']
-        f1.drop('pollutant_unit',axis=1,inplace=True)
-        f2=f1[['state','city','pollutant_avg']]
-        f2.dropna(inplace=True)
+        file=self.airquality()
+        f1=file[file['pollutant']=='CO']
+        x=np.array([])
+        x=np.append(x,f1[f1['pollutant_avg']=='NA'].index.values)
+        f1.drop(x,axis='index',inplace=True)
+        f1=f1.astype({'pollutant_avg':'int64'})
         st.title('Realtime carbon monoxide rate in Indian Cities (refreshes every one hour)')
-        i=st.sidebar.selectbox('Select state',list(np.unique(f2['state'])))
-        c=f2[f2['state']==i]
+        i=st.sidebar.selectbox('Select state',list(np.unique(f1['state'])))
+        c=f1[f1['state']==i]
         fig, ax = plt.subplots(figsize=(7,6))
         plt.title(i)
         ax.scatter(c['city'],c['pollutant_avg'],color='red')
@@ -42,4 +44,3 @@ if __name__ == "__main__":
     air().auto_co_visualizer()
     
       
-
